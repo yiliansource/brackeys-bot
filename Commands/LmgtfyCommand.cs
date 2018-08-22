ceb5f0dee02c0db5e05cbabe9039d091eb9413a7
TheDutchMagikarp <jutudubu@gmail.com>
2018-08-22 12:08:29 +0200

    Add lmgtfy command

---
+++ Commands/LmgtfyCommand.cs
@@ -0,0 +1,33 @@
+﻿using Discord.WebSocket;
+using System.Threading.Tasks;
+using System;
+
+using Discord;
+using Discord.Commands;
+
+namespace BrackeysBot.Commands
+{
+    public class LmgtfyCommand : ModuleBase
+    {
+        public LmgtfyCommand()
+        {
+        }
+
+        [Command("lmgtfy")]
+        [HelpData("lmgtfy <search>", "Performs a google search for a user that doesn't know how to use google")]
+        public async Task GoogleCommand([Remainder] string search)
+        {
+            await ReplyAsync(SearchBuilder(search));
+            return;
+        }
+       
+        private static string SearchBuilder(string search)
+        {
+            string par = search.Replace(" ", "+");
+            string url = "http://lmgtfy.com/?q=" + par;
+            return url;
+        }
+
+
+    }
+}
