311d260fc1082cf0bd75b5fc876b35c5e65c3e95
YilianSource <yiliansource@gmail.com>
2018-09-18 15:12:59 +0200

    Closes #65, introduces custom commands

---
+++ Commands/CustomizedCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Discord;
+using Discord.Commands;
+using System.Text;
+
+namespace BrackeysBot.Commands
+{
+    public class CustomizedCommand : ModuleBase
+    {
+        private CustomizedCommandTable _customCommands;
+
+        public CustomizedCommand(CustomizedCommandTable customCommands)
+        {
+            _customCommands = customCommands;
+        }
+
+        [Command("ccadd")]
+        [HelpData("ccadd <name> <message>", "Adds a command that prints the specified message when called.", AllowedRoles = Data.UserType.Staff)]
+        public async Task AddCustomCommand (string name, [Remainder]string message)
+        {
+            if (_customCommands.Has(name))
+            {
+                _customCommands.Set(name, message);
+            }
+            else
+            {
+                _customCommands.Add(name, message);
+            }
+            await ReplyAsync("Custom command updated.");
+        }
+
+        [Command("ccdelete")]
+        [HelpData("ccdelete <name>", "Deletes the specified custom command.", AllowedRoles = Data.UserType.Staff)]
+        public async Task DeleteCustomCommand (string name)
+        {
+            if (_customCommands.Has(name))
+            {
+                _customCommands.Remove(name);
+
+                await ReplyAsync("Custom command removed.");
+                return;
+            }
+            else
+            {
+                await ReplyAsync("Custom command does not exist.");
+            }
+        }
+    }
+}
