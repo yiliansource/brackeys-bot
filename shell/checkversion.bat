de899ce67f4b6752a5f890d62041a1c2e2874484
CodeMyst <codemyst@outlook.com>
2019-01-01 18:29:18 +0100

    Made the updating work on Linux and Windows

---
+++ shell/checkversion.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+
+SET UPSTREAM=%1
+
+git rev-parse @ > local.txt
+SET /P LOCAL=<local.txt
+DEL local.txt
+
+git rev-parse %UPSTREAM% > remote.txt
+SET /P REMOTE=<remote.txt
+DEL remote.txt
+
+git merge-base @ %UPSTREAM% > base.txt
+SET /P BASE=<base.txt
+DEL base.txt
+
+REM TODO: Make this an if-else
+if %LOCAL% == %REMOTE% (ECHO Up-to-date)
+if %LOCAL% == %BASE% (ECHO Need to pull)
--- shell/update.sh
+++ shell/update.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-# Updates the bot by killing the process, pulling from master and restarting the bot
-# The first argument should be the pid of the bot application
-
-echo "Updating..."
-
-kill $1
-
 git pull https://github.com/YilianSource/brackeys-bot.git
 
-echo "Update completed!"
+echo "Latest changes pulled from git"
 
-dotnet run
+dotnet build > /dev/null
