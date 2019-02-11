6caf741a5eed952dc98eb780401d6a841d6c8e24
YilianSource <yiliansource@gmail.com>
2019-02-11 17:17:50 +0100

    Added user greeting file

---
+++ Data/UserGreetingFile.cs
@@ -0,0 +1,14 @@
+﻿namespace BrackeysBot.Data
+{
+    public class UserGreetingFile : DataFile
+    {
+        public string Message { get; set; }
+
+        public override string FileName => "greeting";
+
+        protected override string SaveToString()
+            => Message;
+        protected override void LoadFromString(string value)
+            => Message = value;
+    }
+}
