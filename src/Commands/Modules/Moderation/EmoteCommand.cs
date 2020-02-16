44215a20f5617406774b8fb122603ec4ffbb7e05
AquaXV <aquaxv@jimune.com>
2020-02-16 14:35:25 +0100

    Removed ChannelExtension, Migrated all IMessageChannel to ITextChannel in favour of Mention over Extension, Removed remark from HideInHelp command

--- src/Commands/Modules/Moderation/EmoteCommand.cs
+++ src/Commands/Modules/Moderation/EmoteCommand.cs
@@ -12,12 +12,11 @@ namespace BrackeysBot.Commands
     {
         [Command("limitemote")]
         [Summary("List the emote limits of the current channel")]
-        [Remarks("limitemote")]
         [RequireModerator]
         [RequireContext(ContextType.Guild)]
         [HideFromHelp]
         public async Task EmoteLimitAsync() 
-            => await EmoteLimitAsync(Context.Channel);
+            => await EmoteLimitAsync(Context.Channel as ITextChannel);
 
         [Command("limitemote")]
         [Summary("List the emote limits of the given channel, or current channel if omitted")]
@@ -25,7 +24,7 @@ namespace BrackeysBot.Commands
         [RequireModerator]
         [RequireContext(ContextType.Guild)]
         public async Task EmoteLimitAsync(
-            [Summary("The channel to display the limits for")] IMessageChannel channel)
+            [Summary("The channel to display the limits for")] ITextChannel channel)
         {
             if (channel == null)
                 throw new ArgumentException("Given channel does not exist");
@@ -36,14 +35,14 @@ namespace BrackeysBot.Commands
             if (limits == null || !limits.ContainsKey(channel.Id) || limits.GetValueOrDefault(channel.Id).Count() == 0)
             {
                 builder.WithColor(Color.Red)
-                    .WithDescription($"No restrictions in {channel.Linkable()}!");
+                    .WithDescription($"No restrictions in {channel.Mention}!");
             } 
             else 
             {
                 List<string> limitedTo = limits.GetValueOrDefault(channel.Id);
 
                 builder.WithColor(Color.Green)
-                    .WithDescription($"Channel {channel.Linkable()} limits to {string.Join(", ", limitedTo)}");
+                    .WithDescription($"Channel {channel.Mention} limits to {string.Join(", ", limitedTo)}");
             }
             
             await builder.Build()
@@ -56,7 +55,7 @@ namespace BrackeysBot.Commands
         [RequireModerator]
         [RequireContext(ContextType.Guild)]
         public async Task AddEmoteLimitAsync(
-            [Summary("The channel to display the limits for")] IMessageChannel channel,
+            [Summary("The channel to display the limits for")] ITextChannel channel,
             [Summary("The emote to allow"), OverrideTypeReader(typeof(EmoteTypeReader))] IEmote emote)
         {
             if (channel == null)
@@ -85,7 +84,7 @@ namespace BrackeysBot.Commands
             limits.Add(channel.Id, newLimits);
 
             await new EmbedBuilder().WithColor(Color.Green)
-                .WithDescription($"Channel {channel.Linkable()} now limits to {string.Join(", ", newLimits.ConvertAll(ConvertToVisual))}")
+                .WithDescription($"Channel {channel.Mention} now limits to {string.Join(", ", newLimits.ConvertAll(ConvertToVisual))}")
                 .Build()
                 .SendToChannel(Context.Channel);
 
@@ -99,7 +98,7 @@ namespace BrackeysBot.Commands
         [RequireModerator]
         [RequireContext(ContextType.Guild)]
         public async Task DeleteEmoteLimitAsync(
-            [Summary("The channel to display the limits for")] IMessageChannel channel,
+            [Summary("The channel to display the limits for")] ITextChannel channel,
             [Summary("The emote to allow"), OverrideTypeReader(typeof(EmoteTypeReader))] IEmote emote)
         {
             if (channel == null)
@@ -111,12 +110,12 @@ namespace BrackeysBot.Commands
             Dictionary<ulong, List<string>> limits = Data.Configuration.EmoteRestrictions;
 
             if (limits == null) 
-                throw new ArgumentException($"Channel {channel.Linkable()} has no emote restrictions");
+                throw new ArgumentException($"Channel {channel.Mention} has no emote restrictions");
 
             List<string> newLimits = limits.GetValueOrDefault(channel.Id, new List<string>());
 
             if (!newLimits.Remove(emote.StringVal()))
-                throw new ArgumentException($"Channel {channel.Linkable()} does not allow emote {emote.Emotify()}");
+                throw new ArgumentException($"Channel {channel.Mention} does not allow emote {emote.Emotify()}");
 
             if (limits.ContainsKey(channel.Id)) 
                 limits.Remove(channel.Id);
@@ -124,7 +123,7 @@ namespace BrackeysBot.Commands
             limits.Add(channel.Id, newLimits);
 
             await new EmbedBuilder().WithColor(Color.Green)
-                .WithDescription($"Channel {channel.Linkable()} now limits to {string.Join(", ", newLimits.ConvertAll(ConvertToVisual))}")
+                .WithDescription($"Channel {channel.Mention} now limits to {string.Join(", ", newLimits.ConvertAll(ConvertToVisual))}")
                 .Build()
                 .SendToChannel(Context.Channel);
 
