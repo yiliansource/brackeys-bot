815b2115532cfe6a7cb54bad26cadddaa97986f2
Gulmorg <mutkudoganay@gmail.com>
2021-11-05 00:27:20 +0300

    Fixed a bug that would split links if they had a "SensitiveCharacter" in them
    SanitizeMarkdown() method would prefix underscored and other sensitive characters in links with a backslash therefore corrupting the link. Added an if check to only prefix the '[' character for sneaky links.

--- src/Services/CollaborationService.cs
+++ src/Services/CollaborationService.cs
@@ -528,7 +528,7 @@ namespace BrackeysBot.Services
                 _collab.DeactivateUser(_message.Author);
                 for (int i = 0; i < _fields.Count; i++)
                 {
-                    _fields[_fields.ElementAt(i).Key] = SanitizeMarkdown(_fields.ElementAt(i).Value);
+                    _fields[_fields.ElementAt(i).Key] = SanitizeMarkdown(_fields.ElementAt(i));
                 }
             }
 
@@ -672,8 +672,15 @@ namespace BrackeysBot.Services
 
                 return string.Join(' ', _portfolioArray);
             }
-            private static string SanitizeMarkdown(string text)
+            private static string SanitizeMarkdown(KeyValuePair<string, string> convo)
             {
+                string text = convo.Value;
+                if (convo.Key == "portfolio")
+                {
+                    text = text.Replace("[", "\\[");
+                    Console.WriteLine("sanitized links");
+                    return text;
+                }
                 foreach (string unsafeChar in SensitiveCharacters)
                     text = text.Replace(unsafeChar, $"\\{unsafeChar}");
                 return text;
